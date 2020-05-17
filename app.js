ca0edc06894c4db0393462c67eb6ec39ce742a1f
wizardstein-dev <dashboy247@gmail.com>
2020-05-17 13:25:41 -0700

    CustomerSupportPage

--- app.js
+++ app.js
@@ -1,52 +1,52 @@
 const express = require("express");
-const expressLayouts = require('express-ejs-layouts');
+const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 const app = express();
-const passport = require('passport');
-const flash = require('connect-flash');
-const session = require('express-session');
+const passport = require("passport");
+const flash = require("connect-flash");
+const session = require("express-session");
 const PORT = process.env.PORT || 3000;
 const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/home", function(req, res) {
-    res.sendFile(__dirname + "/index.html")
-})
-app.get("/login", function(req, res) {
-    res.sendFile(__dirname + "/login.ejs")
-})
+app.get("/home", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/login", function (req, res) {
+  res.sendFile(__dirname + "/login.ejs");
+});
 
-app.get("/signup", function(req, res) {
-    res.sendFile(__dirname + "/register.html")
-})
+app.get("/signup", function (req, res) {
+  res.sendFile(__dirname + "/register.html");
+});
 
-app.get("/payment", function(req, res) {
-    res.sendFile(__dirname + "/payment.html")
-})
+app.get("/payment", function (req, res) {
+  res.sendFile(__dirname + "/payment.html");
+});
 
-app.get("/companyInfo", function(req, res) {
-    res.sendFile(__dirname + "/companyInfo.html")
-})
+app.get("/companyInfo", function (req, res) {
+  res.sendFile(__dirname + "/companyInfo.html");
+});
 
-app.get("/FAQ", function(req, res) {
-    res.sendFile(__dirname + "/Faq.html")
-})
+app.get("/FAQ", function (req, res) {
+  res.sendFile(__dirname + "/Faq.html");
+});
 
-app.get("/contactUs", function(req, res) {
-    res.sendFile(__dirname + "/contactUs.html")
-})
+app.get("/contactUs", function (req, res) {
+  res.sendFile(__dirname + "/contactUs.html");
+});
 
+app.get("/customerSupport", function (req, res) {
+  res.sendFile(__dirname + "/customerSupport.html");
+});
 
-
-
-app.post("/contactUs", function(req, res) {
-
-    const output = `
+app.post("/contactUs", function (req, res) {
+  const output = `
     <h1>You have a new feedback</h1>
     <ul>  
       <h3>Full Name: ${req.body.name}</h3>
@@ -57,80 +57,72 @@ app.post("/contactUs", function(req, res) {
     <h3>${req.body.subject}</h1>
   `;
 
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "sziacustomer@gmail.com",
+      pass: "Sziaszia12",
+    },
+  });
 
+  var mailOptions = {
+    from: req.body.email,
+    to: "TEAMSZIA@GMAIL.COM",
+    subject: "Costumers Email",
+    html: output,
+  };
 
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: "sziacustomer@gmail.com",
-            pass: "Sziaszia12"
-        }
-    });
-
-    var mailOptions = {
-        from: req.body.email,
-        to: 'TEAMSZIA@GMAIL.COM',
-        subject: 'Costumers Email',
-        html: output
-
-    };
-
-    transporter.sendMail(mailOptions, function(error, info) {
-        if (error) {
-            console.log(error);
-            res.sendFile(__dirname + ("/failed.html"), { msg: "email sent" });
-
-
-        } else {
-            console.log('Email sent: ' + info.response);
-            res.sendFile(__dirname + ("/success.html"), { msg: "email sent" });
-        }
-
-    });
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      res.sendFile(__dirname + "/failed.html", { msg: "email sent" });
+    } else {
+      console.log("Email sent: " + info.response);
+      res.sendFile(__dirname + "/success.html", { msg: "email sent" });
+    }
+  });
 });
-//bodyparser 
-app.use(express.urlencoded({ extended: false }))
+//bodyparser
+app.use(express.urlencoded({ extended: false }));
 
 const db = require("./config/keys").mongoURI;
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // Express session
 app.use(
-    session({
-        secret: 'secret',
-        resave: false,
-        saveUninitialized: true
-    })
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  })
 );
 
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-
-// connect flash 
+// connect flash
 app.use(flash());
 
 // Global variables
-app.use(function(req, res, next) {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    next();
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
 });
 
-
-//connect mangodb using mongoose 
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("mongoo bd connected"))
-    .catch(() => console.log(err));
+//connect mangodb using mongoose
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("mongoo bd connected"))
+  .catch(() => console.log(err));
 
 //ejs
 app.use(expressLayouts);
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use("/", require("./routes/index"));
 app.use("/dashboard", require("./routes/index"));
-app.use("/users", require("./routes/users"))
+app.use("/users", require("./routes/users"));
 app.listen(PORT, console.log(`we are live on ${PORT}`));
